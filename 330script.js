f56844297a3c2bc832457c964528638e301ba4d7
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-02-11 14:46:37 -0600

    Update 330script.js

--- 330script.js
+++ 330script.js
@@ -1,3 +1,7 @@
 document.getElementById("txtenter0").onclick = function() {
   document.getElementById("txtenter0").value = "";
 };
+
+document.getElementById("txtenter1").onclick = function() {
+  document.getElementById("txtenter1").value = "";
+};
