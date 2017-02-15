32822ccb3428ff838264f357eb9dcec12dddf8ef
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-02-15 13:31:18 -0600

    Update SignInScript.js

--- SignInScript.js
+++ SignInScript.js
@@ -1,10 +1,19 @@
+var email = true;
+var password = true;
+
   document.getElementById("txtenter0").onclick = function() {
-    document.getElementById("txtenter0").value = "";
-    document.getElementById("txtenter0").color = black;
+    if(email){
+       document.getElementById("txtenter0").value = "";
+       document.getElementById("txtenter0").color = black;
+       email = false;
+    }
   };
 
   document.getElementById("txtenter1").onclick = function() {
-    document.getElementById("txtenter1").value = "";
-    document.getElementById("txtenter1").color = black;
+    if(password){
+        document.getElementById("txtenter1").value = "";
+        document.getElementById("txtenter1").color = black;
+        password = false;
+    }
   };
 
