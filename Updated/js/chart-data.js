6c840c8ef0b9e95f5fc83ce0b44305b42b0d9473
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-02-19 16:43:50 -0600

    Update chart-data.js

--- Updated/js/chart-data.js
+++ Updated/js/chart-data.js
@@ -1,18 +1,8 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 	
 	var lineChartData = {
 			labels : ["January","February","March","April","May","June","July"],
 			datasets : [
-				{
-					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				},
 				{
 					label: "My Second dataset",
 					fillColor : "rgba(48, 164, 255, 0.2)",
@@ -21,7 +11,7 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 					pointStrokeColor : "#fff",
 					pointHighlightFill : "#fff",
 					pointHighlightStroke : "rgba(48, 164, 255, 1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+					data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
 				}
 			]
 
