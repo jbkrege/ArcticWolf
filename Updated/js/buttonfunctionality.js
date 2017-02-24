1299baf5a79c8a3f2ba84580bc7230848df8a964
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-02-23 21:29:21 -0600

    Create buttonfunctionality.js

---
+++ Updated/js/buttonfunctionality.js
@@ -0,0 +1,12 @@
+document.getElementById("Add").onclick = function () {
+    var ul = document.getElementById("sidemenu");
+    var oldTemplate = document.getElementById("teamTemplate");
+    var li = oldTemplate.cloneNode(true);
+    oldTemplate.id = "used";
+    var add = document.getElementById("Add");
+    li.childNodes[1].textContent = "Unnamed Team";
+    li.childNodes[1].href = li.childNodes[1].href + "1";
+    li.childNodes[3].id = li.childNodes[3].id + "1";
+    ul.appendChild(li);
+    ul.appendChild(add);
+};
