b5db8622c6f2d64df61a010126a387f74420e461
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-02-26 16:31:46 -0600

    Update buttonfunctionality.js

--- Updated/js/buttonfunctionality.js
+++ Updated/js/buttonfunctionality.js
@@ -2,7 +2,7 @@ document.getElementById("Add").onclick = function () {
     var ul = document.getElementById("sidemenu");
     var oldTemplate = document.getElementById("teamTemplate");
     var li = oldTemplate.cloneNode(true);
-    oldTemplate.id = "used";
+    oldTemplate.id = (ul.childNodes.length - 5).toString();
     var add = document.getElementById("Add");
     li.childNodes[1].textContent = "Unnamed Team";
     li.childNodes[1].href = li.childNodes[1].href + "1";
@@ -10,4 +10,22 @@ document.getElementById("Add").onclick = function () {
     li.className = "parent";
     ul.appendChild(li);
     ul.appendChild(add);
+};
+
+document.getElementById("NameEdit").onclick = function () {
+    var name = document.getElementById("NameEnter").value;
+    if (name != "") {
+        document.getElementById("NameEnter").value = "";
+        document.getElementById("teamTemplate").childNodes[1].textContent = name;
+    }
+};
+
+document.getElementById("AddMember").onclick = function () {
+    var name = document.getElementById("MemberName").value;
+    document.getElementById("MemberName").value = "";
+    if (name != "") {
+        var childe = document.getElementById("MemberTemplate").cloneNode(true);
+        childe.textContent = name;
+        document.getElementById("MemberList").appendChild(childe);
+    }
 };
