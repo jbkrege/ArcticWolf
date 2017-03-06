2bf2d104529e638db9c5f6d6366323b586bcd391
Jeff0Brewer <jeffreybrewer2019@u.northwestern.edu>
2017-03-06 16:56:42 -0600

    Update buttonfunctionality.js

--- Updated/js/buttonfunctionality.js
+++ Updated/js/buttonfunctionality.js
@@ -1,22 +1,29 @@
+var nextPage;
+var clickedTeam;
+
 document.getElementById("Add").onclick = function () {
+    addTeam("Unnamed Team");
+};
+
+function addTeam(name) {
     var ul = document.getElementById("sidemenu");
     var oldTemplate = document.getElementById("teamTemplate");
     var li = oldTemplate.cloneNode(true);
     oldTemplate.id = (ul.childNodes.length - 5).toString();
     var add = document.getElementById("Add");
-    li.childNodes[1].textContent = "Unnamed Team";
+    li.childNodes[1].textContent = name;
     li.childNodes[1].href = li.childNodes[1].href + "1";
     li.childNodes[3].id = li.childNodes[3].id + "1";
     li.className = "parent";
     ul.appendChild(li);
     ul.appendChild(add);
-};
+}
 
 document.getElementById("NameEdit").onclick = function () {
     var name = document.getElementById("NameEnter").value;
     if (name != "") {
         document.getElementById("NameEnter").value = "";
-        document.getElementById("teamTemplate").childNodes[1].textContent = name;
+        clickedTeam.childNodes[1].textContent = name;
     }
 };
 
@@ -30,20 +37,33 @@ document.getElementById("AddMember").onclick = function () {
     }
 };
 
-function viewStatus() {
-    document.getElementById("teamStatus").className = "";
-    document.getElementById("evaluateTeam").className = "hide";
-    document.getElementById("manageTeam").className = "hide";
+function navSite(e) {
+    window.name = "";
+    var lastclicked = e.parentNode.parentNode.parentNode.id;
+    window.name += lastclicked + "|";
+    window.name += document.getElementById("teamTemplate").childNodes[1].textContent + "|";
+    for (i = 1; i < 20; i++) {
+        window.name += document.getElementById(i.toString()).childNodes[1].textContent + "|";
+    }
 }
 
-function evalTeam() {
-    document.getElementById("teamStatus").className = "hide";
-    document.getElementById("evaluateTeam").className = "";
-    document.getElementById("manageTeam").className = "hide";
-}
-
-function managTeam() {
-    document.getElementById("teamStatus").className = "hide";
-    document.getElementById("evaluateTeam").className = "hide";
-    document.getElementById("manageTeam").className = "";
+function loadUp() {
+    var ind = window.name.indexOf("|");
+    var start = 0;
+    var lastclicked = window.name.substring(start, ind);
+    start = ind + 1;
+    ind = window.name.indexOf("|", start);
+    var last = window.name.substring(start, ind);
+    start = ind + 1;
+    ind = window.name.indexOf("|", start);
+    while (ind > 0) {
+        addTeam(window.name.substring(start, ind));
+        start = ind + 1;
+        ind = window.name.indexOf("|", start);
+    }
+    if (last != "") {
+        addTeam(last);
+    }
+    clickedTeam = document.getElementById(lastclicked);
+    clickedTeam.className = "parent active";
 }
